a8bd2866edc5753bdefa0174ed8ac2c3f52a5b24
lilpado <tyem9721@gmail.com>
2024-12-13 15:17:17 +0900

    파일 입력 구현. TODO: 허프만코딩으로 실제 content 변환

--- HuffmanCoding.cpp
+++ HuffmanCoding.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include <fstream>
+#include <map>
+
 using namespace std;
 
 struct Node
@@ -40,7 +43,28 @@ void PrintCodes(Node* root, string str)
 	PrintCodes(root->right, str + "1");
 }
 
-void HuffmanCoding(vector<char> data, vector<int> freq)
+// TODO:
+void ConvertCodeHelper(Node* root, string str /*map 참조*/) // 실제 압축 변환 재귀
+{
+	if (!root)
+		return;
+
+	if (!root->left && !root->right)
+		// map에 저장
+
+	ConvertCodeHelper(root->left, str + "0");
+	ConvertCodeHelper(root->right, str + "1");
+
+}
+
+void ConvertCode(Node* root, string str /*content 참조*/) // 압축 변환 실행
+{
+	// std::map 생성
+	// ConvertCodeHelper -> map 반환
+	// map을 이용해 실제 content를 변환
+}
+
+void HuffmanCoding(vector<char> data, vector<int> freq /*content 참조*/)
 {
 	// priority: less freq
 	priority_queue<Node*, vector<Node*>, Compare> heap;
@@ -63,13 +87,51 @@ void HuffmanCoding(vector<char> data, vector<int> freq)
 		cout << "(" << left->data << ", " << left->freq << ") + (" << right->data << ", " << right->freq << ") -> ";
 		cout << "(" << top->data << ", " << top->freq << ")" << endl;
 	}
+
 	PrintCodes(heap.top(), "");
+	// TODO: ConvertCode()
+	// TODO: WriteFile()
 }
 
 int main()
 {
-	vector<char> data = { 'a', 'b', 'c', 'd', 'e', 'f' };
-	vector<int> freq = { 45, 13, 12, 16, 9, 5 };
+	// 파일 입력 처리
+	string filename = "The game of life and how to play it by Florence Scovel Shinn.txt";
+	ifstream ifs(filename);
+
+	if (!ifs)
+	{
+		cerr << "File open ERROR" << endl;
+		exit(1);
+	}
+
+	map<char, int, greater<int>> char_count;
+
+	string content;
+	char ch;
+	while (ifs.get(ch))
+	{
+		content += ch;
+		if (char_count.find(ch) == char_count.end()) // 없으면
+			char_count[ch] = 1;
+		else
+			char_count[ch] += 1;
+	}
+
+	std::cout << "File content:\n" << content << std::endl;
+
+	ifs.close();
+
+	// 허프만코딩
+	vector<char> data;
+	vector<int> freq;
+
+	for (auto p : char_count)
+	{
+		data.push_back(p.first);
+		freq.push_back(p.second);
+	}
+
 
 	HuffmanCoding(data, freq);
 
